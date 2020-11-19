617c8c64b6d895504c2344052a0b4d4d48a5ff54
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-11-18 18:52:45 -0800

    Small style changes.

--- src/components/PollContainer.tsx
+++ src/components/PollContainer.tsx
@@ -5,7 +5,7 @@ function PollContainer() {
   return (
     <div className="main-poll">
       <h1>Today's Poll</h1>
-      <h2>This is the question</h2>
+      <h3>This is the question</h3>
     </div>
   );
 }
