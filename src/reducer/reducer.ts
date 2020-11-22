2d6a8717f6bbee5f1c0b9b731663228f48e24b81
Arek Der-Sarkissian <areksds@gmail.com>
2020-11-22 12:14:26 -0800

    Basic structure for redux

---
+++ src/reducer/reducer.ts
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux'
+
+/*
+IMPORT REDUCERS HERE
+
+import { Login } from './Login'
+import { Register } from './Register'
+etc.
+*/
+
+const rootReducer = combineReducers({
+    
+  /* Combine reducers
+  login: Login,
+  register: Register,
+  etc.
+  */
+})
+
+export default rootReducer
