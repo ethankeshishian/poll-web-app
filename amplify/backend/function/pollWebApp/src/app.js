7470189d9da64c49f1bd928c896b5cee04573464
Narek Daduryan <dadur604604@gmail.com>
2020-11-24 21:15:11 -0800

    Add authentication to amplify function
    Enabled pollWebApp function to get authentication
    information from frontend.
    Added middleware to obtain authenticated user username.

--- amplify/backend/function/pollWebApp/src/app.js
+++ amplify/backend/function/pollWebApp/src/app.js
@@ -12,7 +12,6 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-
 const AWS = require("aws-sdk");
 var awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 var bodyParser = require("body-parser");
@@ -34,10 +33,36 @@ const latestPollSuffix = "/latest";
 const singlePollSuffix = "/:poll";
 const respondToPollSuffix = "/respond/:response";
 
+const cognitoUserMiddleware = async (req, res, next) => {
+  try {
+    const IDP_REGEX = /.*\/.*,(.*)\/(.*):CognitoSignIn:(.*)/;
+    const authProvider =
+      req.apiGateway.event.requestContext.identity
+        .cognitoAuthenticationProvider;
+    const [, , , userId] = authProvider.match(IDP_REGEX);
+
+    const cognito = new AWS.CognitoIdentityServiceProvider();
+    const listUsersResponse = await cognito
+      .listUsers({
+        UserPoolId: process.env.AUTH_CS97POLLWEBAPPA7BC17B0_USERPOOLID,
+        Filter: `sub = "${userId}"`,
+        Limit: 1,
+      })
+      .promise();
+    const user = listUsersResponse.Users[0];
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
 // declare a new express app
 var app = express();
 app.use(bodyParser.json());
 app.use(awsServerlessExpressMiddleware.eventContext());
+app.use(cognitoUserMiddleware);
 
 // Enable CORS for all methods
 app.use(function (req, res, next) {
