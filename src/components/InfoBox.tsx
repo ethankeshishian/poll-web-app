48b399c532a1942cfda3595a2b6a07b6f029350c
julia-yin <juliayin@ucla.edu>
2020-12-01 21:41:56 -0800

    Updated info boxes to be fillable by user

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 import '../styles/InfoBox.css';
 
 function InfoBox(props: { subject: string }) {
-  return (<div className="info-box">{props.subject}</div>);
+  return (
+    <form>
+      <label>
+        <input className = "info-box" type="text" placeholder={props.subject}/>
+      </label>
+    </form>
+  );
 }
 
 export default InfoBox;
