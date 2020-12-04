5e8060571ce2f6b1f4fa2cbddf78c67c3270945f
Hannah <hzhong02@gmail.com>
2020-12-04 00:18:26 -0500

    Navbar style update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ import '../styles/Header.css';
 function Header() {
   return (
     <div className="header">
+        <img className="icon" src="logo.png"></img>
         <Link className="header-link heavy left" to="/">POLLIFY</Link>
         <Link className="header-link heavy right" to="/">HOME</Link>
         <Link className="header-link heavy right" to="/profile">ACCOUNT</Link>
