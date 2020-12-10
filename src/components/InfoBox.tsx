dd9144af64ce48c32c1c129c987ab82227b67676
Arek Der-Sarkissian <areksds@gmail.com>
2020-12-10 14:27:20 -0800

    Problem code

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,12 +1,20 @@
-import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
 import '../styles/InfoBox.css';
 
-function InfoBox(props: { subject: string, onChange?: any }) {
+function InfoBox(props: { subject: string, onChange?: any, gender?: boolean, value?: string }) {
   return (
     <form>
       <label>
+        {!props.gender &&
         <input className = "info-box" onChange={props.onChange} type="text" placeholder={props.subject}/>
+        }
+        {props.gender && 
+        <select className = "info-box" value={props.value} onChange={props.onChange}>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+        </select>
+        }
       </label>
     </form>
   );
