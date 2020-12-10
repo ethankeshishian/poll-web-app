623b9ad0ca11d97ad5da413ad409101422a824d2
Ethan Keshishian <ehk2000@sbcglobal.net>
2020-12-10 14:45:59 -0800

    Updated logo to be a link.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,23 @@ import '../styles/Header.css';
 function Header() {
   return (
     <div className="header">
+      <Link className="icon-container" to="/">
         <img className="icon" src="logo.png"></img>
-        <Link className="header-link heavy left" to="/">POLLIFY</Link>
-        <Link className="header-link heavy right" to="/">HOME</Link>
-        <Link className="header-link heavy right" to="/profile">ACCOUNT</Link>
-        <Link className="header-link heavy right" to="/about">ABOUT</Link>
+      </Link>
+      <Link className="header-link heavy left" to="/">
+        POLLIFY
+      </Link>
+      <Link className="header-link heavy right" to="/">
+        HOME
+      </Link>
+      <Link className="header-link heavy right" to="/profile">
+        ACCOUNT
+      </Link>
+      <Link className="header-link heavy right" to="/about">
+        ABOUT
+      </Link>
     </div>
   );
 }
-
 
 export default Header;
--- src/components/PollButton.tsx
+++ src/components/PollButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled, { css, keyframes } from 'styled-components';
 import '../styles/PollButton.css';
 
